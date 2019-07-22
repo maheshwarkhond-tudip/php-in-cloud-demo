0e92e717cc8340410d7eae9e97a6aec31d679015
“maheshwarkhond-tudip” <maheshwar.khond@tudip.com>
2019-07-22 18:33:29 +0530

    Adding index php file

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php include("includes/a_config.php");?>
+<!DOCTYPE html>
+<html>
+<body>
+<div>
+        <img src="img/cloud.png">
+        <h2>Hello Learners, Welcome to the Study Jam Demo Website!</h2>
+        <h4>Let's get to know more about some of the famous Google Cloud Services</h4>
+        <p>
+                <u><b>Google Cloud Storage</b></u> <br> <br>                
+			Google Cloud Storage is a RESTful online file storage web service for storing and accessing data on Google Cloud Platform infrastructure. The service combines the performance and scalability of Google's cloud with advanced security and sharing capabilities. It is an Infrastructure as a Service (IaaS), comparable to Amazon S3 online storage service. Contrary to Google Drive and according to different service specifications, Google Cloud Storage appears to be more suitable for enterprise. <br>
+			Google Storage offers four storage classes, identical in throughput, latency, and durability. The four classes, Multi-Regional Storage, Regional Storage, Nearline Storage, and Coldline Storage, differ in their pricing, minimum storage durations, and availability. <br><br>
+			Learn more about Google Cloud Storage → <a href="https://youtu.be/081hh6EzlTk"> https://youtu.be/081hh6EzlTk </a>
+        </p>
+        <p>
+                <u><b>Google App Engine</b></u> <br><br>
+                
+            Google App Engine (often referred to as GAE or simply App Engine) is a Platform as a Service and cloud computing platform for developing and hosting web applications in Google-managed data centers. Applications are sandboxed and run across multiple servers. App Engine offers automatic scaling for web applications—as the number of requests increases for an application, App Engine automatically allocates more resources for the web application to handle the additional demand. <br>
+            Google App Engine is free up to a certain level of consumed resources and only in the standard environment but not in a flexible environment. Fees are charged for additional storage, bandwidth, or instance hours required by the application.<br><br>
+            Learn more about Google App Engine → <a href="https://youtu.be/2PRciDpqpko"> https://youtu.be/2PRciDpqpko </a>	
+        </p>
+</div>
+</body>
+</html>
